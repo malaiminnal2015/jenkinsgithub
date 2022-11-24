9d3029915569c133fd8f5cb0adc8ba3365acf276
arul murugan <malaiminnal2015@gmail.com>
2022-11-24 12:00:54 -0500

    adding

---
+++ JenkinsWorld3.java
@@ -0,0 +1,11 @@
+package jenkinsexercise;
+
+public class JenkinsWorld {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Hi Varmans, Welcome to the Jenkins World!!!");
+
+	}
+
+}
